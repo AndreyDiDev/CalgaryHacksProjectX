004f153f1c1f614d305733dc2a840cfac231aa80
Emmanuel Trinidad <emmantrin2@gmail.com>
2023-02-18 21:16:34 -0700

    bruh

--- index.js
+++ index.js
@@ -12,32 +12,20 @@
 let btn1 = document.getElementById("option1")
 let btn2 = document.getElementById("option2")
 let btn3 = document.getElementById("option3")
-let btn1Flag = true;
-let btn2Flag = false;
-let btn3Flag = false;
-btn1.style.color = "yellow";
 
-btn1.addEventListener('click', function(){
+btn1.addEventListener('mouseover', function(){
     btn1.style.color = "yellow";
     btn2.style.color = "black";
     btn3.style.color = "black";
-    btn1Flag = true;
-    btn2Flag = false;
-    btn3Flag = false;
+
 })
-btn2.addEventListener('click', function(){
+btn2.addEventListener('mouseover', function(){
     btn2.style.color = "yellow";
     btn1.style.color = "black";
     btn3.style.color = "black";
-    btn2Flag = true;
-    btn1Flag = false;
-    btn3Flag = false;
 })
-btn3.addEventListener('click', function(){
+btn3.addEventListener('mouseover', function(){
     btn3.style.color = "yellow";
     btn2.style.color = "black";
     btn1.style.color = "black";
-    btn3Flag = true;
-    btn1Flag = false;
-    btn2Flag = false;
 })
